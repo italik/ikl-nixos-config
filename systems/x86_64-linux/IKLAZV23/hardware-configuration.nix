8b28298fd802162bfb3e4a38716550771c8b108e
Ben Standerline <ben.standerline@italik.co.uk>
2024-04-05 15:43:38 +0100

    Add /data mount to IKLAZV23 hardware-configuration

--- systems/x86_64-linux/IKLAZV23/hardware-configuration.nix
+++ systems/x86_64-linux/IKLAZV23/hardware-configuration.nix
@@ -21,6 +21,11 @@
       fsType = "vfat";
     };
 
+  fileSystems."/data" =
+    { device = "/dev/disk/by-label/data";
+      fsType = "ext4";
+    };
+
   swapDevices =
     [ { device = "/dev/disk/by-label/swap"; }
     ];
