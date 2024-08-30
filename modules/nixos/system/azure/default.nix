8db6cacb2b5d59c420ae36d65465fb69a6ba6f0d
Ben Standerline <ben.standerline@italik.co.uk>
2024-08-30 17:24:51 +0100

    Add fix for waagent

--- modules/nixos/system/azure/default.nix
+++ modules/nixos/system/azure/default.nix
@@ -264,10 +264,31 @@ in {
         # How often (in seconds) to monitor for DHCP client restarts
         OS.MonitorDhcpClientRestartPeriod=30
     '';
-  environment.persistence."/data" = {
-    directories = [
-      "/var/lib/waagent"
-    ];
-  };
+    environment.persistence."/data" = {
+      directories = [
+        "/var/lib/waagent"
+      ];
+    };
+    # Workaround buggy Azure Linux agent
+    systemd.timers.create_waagent_symlinks = {
+      wantedBy = [ "timers.target" ];
+      timerConfig = {
+        OnBootSec = "5m";
+        Unit = "create_waagent_symlinks.service";
+      };
+    };
+    systemd.services.create_waagent_symlinks = {
+      serviceConfig = {
+        Type = "oneshot";
+        User = "root";
+      };
+      script = ''
+        ln -sf /run/current-system/sw/bin/base64 /usr/bin/base64
+        ln -sf ${pkgs.openssl}/bin/openssl /usr/bin/openssl
+        ln -sf /run/wrappers/bin/mount /bin/mount
+        mkdir -p /usr/sbin
+        ln -sf ${pkgs.waagent}/bin/waagent /usr/sbin/waagent
+      '';
+    };
   };
 }
