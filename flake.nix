e3d6d3f98a615048ddd3dc2007dfe86a03573c62
Ben Standerline <ben.standerline@italik.co.uk>
2024-10-10 12:44:26 +0100

    Use testing nixpkgs

--- flake.nix
+++ flake.nix
@@ -3,7 +3,8 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-24.05";
-    unstable.url = "github:nixos/nixpkgs/nixos-unstable";
+    #unstable.url = "github:nixos/nixpkgs/nixos-unstable";
+    unstable.url = "github:bstanderline/nixpkgs/master";
 
     snowfall-lib = {
       url = "github:snowfallorg/lib";
