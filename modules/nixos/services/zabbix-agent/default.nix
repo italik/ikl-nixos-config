7517c9e8ac9215a95bdda286db79b203fa119571
Ben <ben@benstanderline.com>
2025-01-20 11:21:34 +0000

    Add serverActive option to Zabbix-Agent module

--- modules/nixos/services/zabbix-agent/default.nix
+++ modules/nixos/services/zabbix-agent/default.nix
@@ -5,7 +5,8 @@ with lib.ikl; let
 in {
   options.ikl.services.zabbix-agent = with types; {
     enable = mkBoolOpt false "Whether or not to enable Zabbix Agent.";
-    server = mkOpt str "zabbix.italikintra.net" "Zabbix Server or Proxy to connect to.";
+    server = mkOpt str "zabbix.italikintra.net" "Zabbix Server or Proxy to connect to for Passive checks.";
+    serverActive = mkOpt str cfg.server "Zabbix Server or Proxy to connect to for Active checks";
   };
 
   config = mkIf cfg.enable {
@@ -14,6 +15,9 @@ in {
       package = pkgs.zabbix70.agent2;
       openFirewall = true;
       server = cfg.server;
+      settings = {
+        ServerActive = cfg.serverActive;
+      };
     };
   };
 }
