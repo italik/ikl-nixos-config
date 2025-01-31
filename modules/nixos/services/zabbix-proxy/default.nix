5e89f4ec95e5a55dac0a1c6d3be825f71c4ae6d7
Ben <ben@benstanderline.com>
2025-01-31 13:05:52 +0000

    Create initial zabbix proxy production deployment

--- modules/nixos/services/zabbix-proxy/default.nix
+++ modules/nixos/services/zabbix-proxy/default.nix
@@ -2,17 +2,27 @@
 with lib;
 with lib.ikl; let
   cfg = config.ikl.services.zabbix-proxy;
+  pskPath = "/data/secrets/zabbix-proxy-psk.key";
 in {
   options.ikl.services.zabbix-proxy = with types; {
     enable = mkBoolOpt false "Whether or not to enable Zabbix Proxy.";
   };
 
   config = mkIf cfg.enable {
+    # Allow SSH inbound
     networking.firewall.allowedTCPPorts = [ 22 ];
+
+    # Configure Zabbix Proxy and allow Zabbix proxy inbound connections
     services.zabbixProxy = {
       enable = true;
       package = pkgs.zabbix70.proxy-pgsql;
       extraPackages = []; # List of pkgs
+      settings = {
+        TLSAccept = "psk";
+        TLSConnect = "psk";
+        TLSPSKFile = pskPath;
+        TLSPSKIdentity = networking.hostname;
+      };
       database = {
         createLocally = true;
         host = ""; # Use Unix socket
@@ -26,6 +36,7 @@ in {
       server = "zabbix.italikintra.net";
     };
 
+    # Installl the Zabbix Agent so we can monitor the proxy itself
     services.zabbixAgent = {
       enable = true;
       package = pkgs.zabbix70.agent2;
@@ -33,6 +44,7 @@ in {
       server = "127.0.0.1";
     };
 
+    # Configure the database that Zabbix proxy uses
     services.postgresql.ensureDatabases = [ "zabbix" ];
     services.postgresql.ensureUsers = [
       {
@@ -40,6 +52,22 @@ in {
         ensureDBOwnership = true;
       }
     ];
+
+    # Generate PSK file if it doesn't exist
+    systemd.services.generate-zabbix-psk = {
+      description = "Generate Zabbix PSK if it doesn't exist";
+      wantedBy = [ "multi-user.target" ];
+      before = [ "zabbix-proxy.service" ];
+      script = ''
+        set -eu
+        [ ! -f ${pskPath} ] && openssl rand -hex 32 > ${pskPath} && chmod 400 ${pskPath} && chown zabbix:zabbix ${pskPath}
+      '';
+      serviceConfig = {
+        Type = "oneshot";
+        User = "root";
+        RemainAfterExit = true;
+      };
+    };
     
   };
 }
