dbf6fa0a4375d07302e272834a393712e69fb06b
bstanderline <153822813+bstanderline@users.noreply.github.com>
2025-03-25 11:35:50 +0000

    Create new syslog module

---
+++ modules/nixos/system/syslog/default.nix
@@ -0,0 +1,18 @@
+{ options, config, lib, pkgs, ... }:
+with lib;
+with lib.ikl; let
+  cfg = config.ikl.system.syslog;
+in {
+  options.ikl.system.syslog = with types; {
+    enable = mkBoolOpt false "Whether or not to enable syslog forwarding.";
+  };
+
+  config = mkIf cfg.enable {
+    services.rsyslogd = {
+      enable = true;
+      defaultConfig = ''
+        *.* action(type="omfwd" target="syslog.italikintra.net" port="2514" protocol="tcp" action.resumeRetryCount="100" queue.type="linkedList" queue.size="10000")
+      '';
+    };
+  };
+}
