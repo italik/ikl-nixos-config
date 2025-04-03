55ab4f8759a102f3f59eb933e3011d18e363df4c
bstanderline <153822813+bstanderline@users.noreply.github.com>
2025-04-03 11:31:58 +0100

    Initial grafana module config

---
+++ modules/nixos/services/grafana/default.nix
@@ -0,0 +1,78 @@
+{ options, config, lib, pkgs, ... }:
+with lib;
+with lib.ikl; let
+  cfg = config.ikl.services.grafana;
+in {
+  options.ikl.services.grafana = with types; {
+    enable = mkBoolOpt false "Whether or not to enable Grafana.";
+    vhost = mkOpt str "" "Primary FQDN.";
+  };
+
+  config = mkIf cfg.enable {
+    networking.firewall.allowedTCPPorts = [ 22 443 80 ];
+
+    # Grafana config
+    services.grafana = {
+      enable = true;
+      settings = {
+        server = {
+          protocol = "socket";
+          enforce_domain = true;
+          root_url = cfg.vhost;
+          enforce_domain = true;
+        };
+        database = {
+          user = "grafana";
+          type = "postgres";
+          name = "grafana";
+          host = "/run/postgresql";
+        };
+      };
+    };
+
+    # PostgreSQL config
+    services.postgresql.ensureDatabases = [ "grafana" ];
+    services.postgresql.ensureUsers = [
+      {
+        name = "grafana";
+        ensureDBOwnership = true;
+      }
+    ];
+    services.postgresql.authentication = ''
+      local grafana grafana peer
+    '';
+
+    # Nginx config
+    services.nginx = {
+      enable = true;
+
+      recommendedGzipSettings = true;
+      recommendedOptimisation = true;
+      recommendedTlsSettings = true;
+
+      virtualHosts."netbox.italikintra.net" = {
+        enableACME = true;
+        forceSSL = true;
+        kTLS = true;
+        locations."/" = {
+          proxyPass = "http://unix:/run/grafana/grafana.sock";
+          proxyWebsockets = true;
+        };
+      };
+    };
+    users.users.nginx.extraGroups = [ "grafana" ];
+
+    # ACME config
+    security.acme.acceptTerms = true;
+    security.acme.defaults.email = "alerts@italik.co.uk";
+
+    # Impermanence config
+    environment.persistence."/data" = {
+      directories = [
+        "/var/lib/acme"
+        "/var/lib/grafana"
+        "/var/lib/postgresql"
+      ];
+    };
+  };
+}
