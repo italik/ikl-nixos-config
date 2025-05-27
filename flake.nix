dd786e57388a61c6ac10fd466a8ef1199bb1866c
bstanderline <153822813+bstanderline@users.noreply.github.com>
2025-05-27 15:07:11 +0100

    nixos: 24.11 -> 25.05

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "A very basic flake";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-24.11";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-25.05";
     unstable.url = "github:nixos/nixpkgs/nixos-unstable";
 
     snowfall-lib = {
