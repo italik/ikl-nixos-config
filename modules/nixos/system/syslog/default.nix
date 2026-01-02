f9eb74410c108b41b7de3434fdb74cb7a4eecf5b
bstanderline <153822813+bstanderline@users.noreply.github.com>
2026-01-02 11:39:10 +0000

    syslog: Add sftpgo syslog forward & restructure

--- modules/nixos/system/syslog/default.nix
+++ modules/nixos/system/syslog/default.nix
@@ -2,9 +2,11 @@
 with lib;
 with lib.ikl; let
   cfg = config.ikl.system.syslog;
+  dest = "syslog.italikintra.net";
 in {
   options.ikl.system.syslog = with types; {
     enable = mkBoolOpt false "Whether or not to enable syslog forwarding.";
+    sftpgo = mkBoolOpt false "Whether or not to enable forwarding of sftpgo logs.";
   };
 
   config = mkIf cfg.enable {
@@ -12,8 +14,47 @@ in {
       enable = true;
       defaultConfig = ''
         global(DefaultNetstreamDriverCAFile="/etc/ssl/certs/ca-bundle.crt")
-        *.* action(type="omfwd" target="syslog.italikintra.net" port="2514" protocol="tcp" StreamDriver="gtls" StreamDriverMode="1" action.resumeRetryCount="100" queue.type="linkedList" queue.size="10000")
+
+        ${lib.optionalString cfg.sftpgo ''
+          # SFTPGo logs to alternative port
+          if ($syslogtag == "sftpgo") then {
+            action(
+              type="omfwd"
+              target="${dest}"
+              port="4514"
+              protocol="tcp"
+              StreamDriver="gtls"
+              StreamDriverMode="1"
+              action.resumeRetryCount="100"
+              queue.type="linkedList"
+              queue.size="10000"
+            )
+            stop
+          }
+        ''}
+
+        *.* action(
+            type="omfwd"
+            target="${dest}"
+            port="2514"
+            protocol="tcp"
+            StreamDriver="gtls"
+            StreamDriverMode="1"
+            action.resumeRetryCount="100"
+            queue.type="linkedList"
+            queue.size="10000"
+        )
+      '';
+      extraConfig = mkIf cfg.sftpgo ''
+        input(type="imfile"
+              File="/var/log/sftpgo/sftpgo.log"
+              Tag="sftpgo"
+              Severity="info"
+              Facility="ftp")
       '';
     };
+
+    systemd.services.sftpgo.environment.SFTPGO_LOG_FILE_PATH = mkIf cfg.sftpgo "/var/log/sftpgo/sftpgo.log";
+
   };
 }
