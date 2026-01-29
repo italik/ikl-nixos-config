f0f9410f4b217f82712e4008d32af770b7964547
bstanderline <153822813+bstanderline@users.noreply.github.com>
2026-01-29 15:00:02 +0000

    netbox: Add overlay for 4.5

--- overlays/netbox/default.nix
+++ overlays/netbox/default.nix
@@ -4,4 +4,12 @@ final: prev:
 
 {
   inherit (channels.unstable) netbox;
+  netbox_4_5 = prev.netbox.overrideAttrs (old: {
+    src = prev.fetchFromGitHub {
+      owner = "netbox-community";
+      repo = "netbox";
+      tag = "v4.5.1";
+      hash = "sha256-d/Ne6twYrjmDdEcLwxol+vU+aqvTNnxka6je5nWxDas=";
+    };
+  });
 }
