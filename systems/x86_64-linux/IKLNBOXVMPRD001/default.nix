a42a6a6c684e4026042b5945c9032e68b0cddb8d
bstanderline <153822813+bstanderline@users.noreply.github.com>
2026-01-30 10:31:42 +0000

    IKLNBOXVMPRD001: Override to 4.5

--- systems/x86_64-linux/IKLNBOXVMPRD001/default.nix
+++ systems/x86_64-linux/IKLNBOXVMPRD001/default.nix
@@ -65,6 +65,7 @@ with lib.ikl; {
     services = {
       netbox = {
         enable = true;
+        package = pkgs.netbox_4_5;
         vhost = "netbox.italikintra.net";
         acme.enable = false;
         sslCertificate = "/data/secrets/certificate.pem";
