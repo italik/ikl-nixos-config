a386e8c5006bc9589108abe972d43e9f811d2577
bstanderline <153822813+bstanderline@users.noreply.github.com>
2026-02-03 14:32:02 +0000

    netbox_4_5: 4.5.1 -> 4.5.2

--- overlays/netbox/default.nix
+++ overlays/netbox/default.nix
@@ -8,8 +8,8 @@ final: prev:
     src = prev.fetchFromGitHub {
       owner = "netbox-community";
       repo = "netbox";
-      tag = "v4.5.1";
-      hash = "sha256-d/Ne6twYrjmDdEcLwxol+vU+aqvTNnxka6je5nWxDas=";
+      tag = "v4.5.2";
+      hash = "sha256-JGW7lw10mbYfU5C6+2GWalzIOTzo7nEs+MV3jM1vgIA=";
     };
   });
 }
