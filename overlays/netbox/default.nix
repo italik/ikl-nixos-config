a2e885362c74e8ee84e00992438e5731ce0e2d81
bstanderline <153822813+bstanderline@users.noreply.github.com>
2026-04-27 12:04:18 +0100

    netbox: Resolve django error

--- overlays/netbox/default.nix
+++ overlays/netbox/default.nix
@@ -1,7 +1,19 @@
-{ channels, ... }:
+{ lib, channels, ... }:
 
 final: prev:
 
 {
-  inherit (channels.unstable) netbox;
+  netbox = lib.makeOverridable (
+    args:
+    (channels.unstable.netbox.override args).overrideAttrs (old: {
+      postPatch = (old.postPatch or "") + ''
+        for f in netbox/templates/inc/table.html \
+                 netbox/templates/inc/table_htmx.html; do
+          [ -f "$f" ] && substituteInPlace "$f" \
+            --replace "querystring table.prefixed_order_by_field=" \
+                      "querystring_replace table.prefixed_order_by_field="
+        done
+      '';
+    })
+  ) { };
 }
