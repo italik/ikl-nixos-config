546bfd66dc8149ed02db2ab7b6d2bd2e569d2075
bstanderline <153822813+bstanderline@users.noreply.github.com>
2026-06-25 14:55:34 +0100

    netbox: Switch to container-based install

--- modules/nixos/services/netbox/default.nix
+++ modules/nixos/services/netbox/default.nix
@@ -16,26 +16,75 @@ in {
   config = mkIf cfg.enable {
     networking.firewall.allowedTCPPorts = [ 22 443 80 ];
 
-    services.netbox = {
-      enable = true;
-      package = cfg.package;
-      listenAddress = "[::1]";
-      secretKeyFile = "/data/secrets/netboxSecret";
-      settings = {
-        ALLOWED_HOSTS = [
-          "[::1]"
-          "${cfg.vhost}"
-        ];
-        CSRF_TRUSTED_ORIGINS = [ "https://${cfg.vhost}" ];
-        DISK_BASE_UNIT = 1024;
-        RAM_BASE_UNIT = 1024;
+    virtualisation.podman.defaultNetwork.settings.dns_enabled = true;
+    virtualisation.oci-containers = {
+      containers = {
+        netbox = {
+          image = "netboxcommunity/netbox:latest";
+          environment = {
+            DB_HOST = "postgres";
+            DB_NAME = "netbox";
+            DB_USER = "netbox";
+            DB_WAIT_DEBUG = "1";
+            REDIS_CACHE_DATABASE = "1";
+            REDIS_CACHE_HOST= "valkey-cache";
+            REDIS_CACHE_INSECURE_SKIP_TLS_VERIFY = "false";
+            REDIS_CACHE_SSL = "false";
+            REDIS_DATABASE = "0";
+            REDIS_HOST = "valkey";
+            REDIS_INSECURE_SKIP_TLS_VERIFY = "false";
+            REDIS_SSL = "false";
+          };
+          environmentFiles = [
+            "/data/secrets/postgres_password"
+            "/data/secrets/netboxSecret"
+          ];
+          dependsOn = [
+            "postgres"
+            "valkey"
+            "valkey-cache"
+          ];
+          ports = [
+            "127.0.0.1:8080:8080"
+          ];
+          pull = "newer";
+          volumes = [
+            "/data/netbox/config:/etc/netbox/config:z,ro"
+            "/data/netbox/media:/opt/netbox/netbox/media:rw"
+            "/data/netbox/reports:/opt/netbox/netbox/reports:rw"
+            "/data/netbox/scripts:/opt/netbox/netbox/scripts:rw"
+          ];
+        };
+        postgres = {
+          image = "docker.io/postgres:18-alpine";
+          environment = {
+            POSTGRES_DB = "netbox";
+            POSTGRES_USER = "netbox";
+          };
+          environmentFiles = [
+            "/data/secrets/postgres_password"
+          ];
+          volumes = [
+            "/data/netbox/database:/var/lib/postgresql"
+            "/mnt/resource:/data"
+          ];
+        };
+        valkey = {
+          image = "docker.io/valkey/valkey:9.0-alpine";
+          cmd = [
+            "--appendonly yes"
+          ];
+          volumes = [
+            "/data/netbox/valkey:/data"
+          ];
+        };
+        valkey-cache = {
+          image = "docker.io/valkey/valkey:9.0-alpine";
+          volumes = [
+            "/data/netbox/valkey-cache:/data"
+          ];
+        };
       };
-      extraConfig = ''
-        with open("${cfg.apiTokenPeppersFile}", "r") as pepper_file:
-            API_TOKEN_PEPPERS = {
-                1: pepper_file.readline(),
-            };
-      '';
     };
 
     services.nginx = {
@@ -53,22 +102,10 @@ in {
         forceSSL = true;
         kTLS = true;
         locations."/" = {
-          proxyPass = "http://[::1]:8001";
+          proxyPass = "http://127.0.0.1:8080";
           proxyWebsockets = true;
         };
-        locations."/static/".root = "/var/lib/netbox";
       };
-    };
-    users.users.nginx.extraGroups = [ "netbox" ];
-    
-    security.acme.acceptTerms = cfg.acme.enable;
-    security.acme.defaults.email = mkIf cfg.acme.enable "alerts@italik.co.uk";
-    # Setup folders (see https://github.com/nix-community/impermanence)
-    environment.persistence."/data" = {
-      directories = [
-        "/var/lib/netbox"
-        "/var/lib/postgresql"
-      ] ++ lib.optional cfg.acme.enable "/var/lib/acme";
     };
   };
 }
